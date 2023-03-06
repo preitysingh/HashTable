f3fb1c7b602f90689548da0a3f49a28e98223fc8
preitysingh <111521777+preitysingh@users.noreply.github.com>
2023-03-05 18:15:20 -0700

    put and others

--- HashTable/src/HashTable.java
+++ HashTable/src/HashTable.java
@@ -1,22 +1,36 @@
+import java.util.ArrayList;
+
 //created by Preity Singh
 public class HashTable {
+    public int size =0;
+    private ArrayList<String> hashTable;
+
     //initializes an array of size capacity
     public HashTable(int capacity) {
-        //array!
+        hashTable = new ArrayList<>();
+        for(int i = 0; i < capacity; i++){
+            hashTable.add(null);
+        }
+        size = capacity;
     }
 
     //put hashes the key to an index in your array, and places the value there. Fails if there are collisions/repeat keys.
     public boolean put(String key, String value) {
-        return false;
+        if(get(key) == null) {
+            hashTable.set(Integer.parseInt(key), value);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //get hashes the key to get the index, and returns that element. Returns null if key not found.
     public String get(String key) {
-        return "one";
+        return hashTable.get(Integer.parseInt(key));
     }
 
     //returns the unique int in the range of the [0, array length)
     private int hashCode(String key) {
-        return 0;
+        //hashcode here
     }
 }
